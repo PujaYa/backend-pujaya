f5ee933bf756384bcfab3e2c44dca06e17af5686
omar <omarenriquecs@gmail.com>
2025-06-10 23:06:23 -0400

    error swagger

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,22 @@ async function bootstrap() {
     .build()
 
   const document = SwaggerModule.createDocument(app, confitSwagger)
-  SwaggerModule.setup('api', app, document)
+  SwaggerModule.setup('api', app, document, {
+    customCssUrl: 'https://backend-pujaya-pehvtekn4-pujaycoms-projects.vercel.app/docs/swagger-ui.css',
+    customJs: [
+      'https://backend-pujaya-pehvtekn4-pujaycoms-projects.vercel.app/api/swagger-ui-bundle.js',
+      'https://backend-pujaya-pehvtekn4-pujaycoms-projects.vercel.app/api/swagger-ui-standalone-preset.js',
+      'https://backend-pujaya-pehvtekn4-pujaycoms-projects.vercel.app/api/swagger-ui-init.js'
+    ],
+    // Asegúrate de incluir los favicons también si quieres que se carguen
+    customfavIcon: 'https://backend-pujaya-pehvtekn4-pujaycoms-projects.vercel.app/api/favicon-32x32.png',
+    customCss: '', // Puedes dejar esto vacío si no tienes CSS personalizado inyectado
+    swaggerOptions: {
+      url: '/api/api-json' // Esta es la ruta para tu JSON de la especificación, probablemente sigue siendo /api/api-json si tu prefijo global es /api
+                          // Si tu JSON de swagger lo sirves en /api-json, entonces podría ser así
+                          // Pero si tu app está en /api, y la especificación es /api/api-json, entonces está bien.
+    }
+  })
 
   const port = process.env.PORT ?? 3001;
   await app.listen(port);
